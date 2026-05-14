fe3847c41cba16bbf7232093e7d9cb21d42dcb2c
William S. <williamroberto.silva@hotmail.com>
2026-05-13 21:07:47 -0300

    parte 2

--- src/chat.py
+++ src/chat.py
@@ -1,13 +1,18 @@
 from search import search_prompt
 
 def main():
-    chain = search_prompt()
+    #PEDE A PERGUNTA DO USUÁRIO
+    pergunta = input("O que deseja buscar no documento? ").strip()
 
-    if not chain:
+    if not pergunta:
+        print("Você não digitou nada. Tente novamente.")
+    else:
+        busca = search_prompt(pergunta)
+        print(busca)
+
+    if not busca:
         print("Não foi possível iniciar o chat. Verifique os erros de inicialização.")
         return
     
-    pass
-
 if __name__ == "__main__":
     main()
--- src/search.py
+++ src/search.py
@@ -1,6 +1,14 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import OpenAIEmbeddings
+from langchain_postgres import PGVector
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+
+###########################
 PROMPT_TEMPLATE = """
 CONTEXTO:
-{contexto}
+{results}
 
 REGRAS:
 - Responda somente com base no CONTEXTO.
@@ -24,6 +32,39 @@ PERGUNTA DO USUÁRIO:
 
 RESPONDA A "PERGUNTA DO USUÁRIO"
 """
+###########################
 
-def search_prompt(question=None):
-    pass
+#CARREGA VARIAVEL DE AMBIENTE DO .env
+load_dotenv()
+    
+def search_prompt(pergunta):
+  #INICIA A BUSCA  
+  print(f"Buscando por: '{pergunta}'...")
+
+  #DEFINE MODELO DE EMBEDDING E CONECTA AO POSTGRES PARA REALIZAR A BUSCA DE SIMILARIDADE
+  embeddings = OpenAIEmbeddings(model=os.getenv("OPENAI_MODEL","text-embedding-3-small"))
+
+  store = PGVector(
+      embeddings=embeddings,
+      collection_name=os.getenv("PGVECTOR_COLLECTION"),
+      connection=os.getenv("PGVECTOR_URL"),
+      use_jsonb=True,
+  )
+
+  results = store.similarity_search_with_score(pergunta, k=10)
+
+  # Formata o contexto de forma limpa para a LLM
+  context_text = "\n\n---\n\n".join([res[0].page_content for res in results])
+
+  # Preparação do Prompt 
+  chat_prompt = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+
+  final_prompt = chat_prompt.format(results=context_text, pergunta=pergunta)
+
+  model = ChatOpenAI(model="gpt-5-nano", temperature=0.3)
+  response = model.invoke(final_prompt)
+  return response.content
+
+
+
+
